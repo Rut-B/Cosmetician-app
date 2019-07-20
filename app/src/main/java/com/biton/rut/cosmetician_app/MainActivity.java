f96181384891154a8177a29e73695709193a3295
Rut-B <rutibiton316@gmail.com>
2019-07-20 23:55:38 +0300

    navigation page

---
+++ app/src/main/java/com/biton/rut/cosmetician_app/MainActivity.java
@@ -0,0 +1,26 @@
+package com.biton.rut.cosmetician_app;
+
+import android.content.Intent;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+    private final int SPLASH_TIME_OUT = 700;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Intent resultIntent = new Intent(this, Main2Activity.class);
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent in = new Intent(MainActivity.this, Main2Activity.class);
+                startActivity(in);
+                finish();
+            }
+        }, SPLASH_TIME_OUT);
+    }
+}
